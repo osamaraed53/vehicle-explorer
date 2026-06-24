b74537d6723cd2e0bea454e0b0fc5602b880b0e8
Osama Raed <osamaraed53@gmail.com>
2026-06-24 23:04:41 +0300

    Add the Nhatsa client interface with implementation class

---
+++ backend/VehicleExplorer/VehicleExplorer.Api/Clients/INhtsaClient.cs
@@ -0,0 +1,13 @@
+﻿using VehicleExplorer.Api.Clients.NhtsaResponse;
+
+namespace VehicleExplorer.Api.Clients;
+
+public interface INhtsaClient
+{
+    Task<IReadOnlyList<MakeResponse>> GetAllMakesAsync();
+
+    Task<IReadOnlyList<VehicleTypeResponse>> GetVehicleTypesForMakeAsync(int makeId);
+
+    Task<IReadOnlyList<ModelResponse>> GetModelsAsync(int makeId, int year, string? vehicleTypet);
+
+}
---
+++ backend/VehicleExplorer/VehicleExplorer.Api/Clients/NhtsaClient.cs
@@ -0,0 +1,21 @@
+﻿using VehicleExplorer.Api.Clients.NhtsaResponse;
+
+namespace VehicleExplorer.Api.Clients;
+
+public class NhtsaClient : INhtsaClient
+{
+    public Task<IReadOnlyList<MakeResponse>> GetAllMakesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<ModelResponse>> GetModelsAsync(int makeId, int year, string? vehicleTypet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<VehicleTypeResponse>> GetVehicleTypesForMakeAsync(int makeId)
+    {
+        throw new NotImplementedException();
+    }
+}
