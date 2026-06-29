fca71227e2c9539f1eb7ac8ac94e1b98096a0b89
Osama Raed <osamaraed53@gmail.com>
2026-06-29 11:27:24 +0300

    Initial implement custom exception handler

--- backend/VehicleExplorer/VehicleExplorer.Api/Middlewares/Handler/CustomExceptionHandler.cs
+++ backend/VehicleExplorer/VehicleExplorer.Api/Middlewares/Handler/CustomExceptionHandler.cs
@@ -1,11 +1,46 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using VehicleExplorer.Api.Exceptions;
 
 namespace VehicleExplorer.Api.Middlewares.Handler;
 
 public class CustomExceptionHandler : IExceptionHandler
 {
-    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        (string Detail, string Title, int StatusCode) = exception switch
+        {
+            ValidationException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            _ =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = Title,
+            Detail = Detail,
+            Status = StatusCode,
+            Instance = context.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+        {
+            problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+        }
+
+        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+
+        return true;
     }
 }
