254730cfd548b8a29ad25ceae703abd25a145434
Osama Raed <osamaraed53@gmail.com>
2026-06-29 10:24:38 +0300

    Add Vehicle type slice

---
+++ backend/VehicleExplorer/VehicleExplorer.Api/Vehicle/GetVehicleTypes/GetVehicleTypesEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using MediatR;
+
+namespace VehicleExplorer.Api.Vehicle.GetVehicleTypes;
+
+public class GetVehicleTypesEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("makes/{makeId:int}/vehicle-types", async ([AsParameters] GetVehicleTypesRequest request, ISender sender) =>
+        {
+
+            var result = await sender.Send(request);
+
+            return Results.Ok(result);
+
+        }).WithName("Get all vehicle types")
+        .Produces<GetVehicleTypesResponse>(StatusCodes.Status200OK)
+        .WithSummary("Get all vehicle types")
+        .WithDescription("Get all vehicle types");
+
+    }
+}
---
+++ backend/VehicleExplorer/VehicleExplorer.Api/Vehicle/GetVehicleTypes/GetVehicleTypesHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VehicleExplorer.Api.DTOs;
+using VehicleExplorer.Api.Sevices;
+
+namespace VehicleExplorer.Api.Vehicle.GetVehicleTypes;
+
+
+public record GetVehicleTypesRequest([FromRoute] int MakeId, int Page, int PageSize) : IRequest<GetVehicleTypesResponse>;
+
+public record GetVehicleTypesResponse(IReadOnlyList<VehicleTypeDTO> Data , int Count);
+
+public class GetVehicleTypesHandler(IVehicleService vehicleService) : IRequestHandler<GetVehicleTypesRequest, GetVehicleTypesResponse>
+{
+    public async Task<GetVehicleTypesResponse> Handle(GetVehicleTypesRequest request, CancellationToken cancellationToken)
+    {
+        var entites = await vehicleService.GetVehicleTypesAsync(request.MakeId,cancellationToken);
+
+        var data = entites.Skip((request.Page - 1) * request.PageSize).Take(request.PageSize);
+
+        return new GetVehicleTypesResponse([.. data], entites.Count);
+    }
+}
