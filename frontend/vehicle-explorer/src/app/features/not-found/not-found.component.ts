146147ec2495ec157e6c995e3688fa219fd22445
Osama Raed <osamaraed53@gmail.com>
2026-06-30 20:29:45 +0300

    Build not found component

--- frontend/vehicle-explorer/src/app/features/not-found/not-found.component.ts
+++ frontend/vehicle-explorer/src/app/features/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
 
